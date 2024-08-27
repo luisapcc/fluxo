552f1abae77ccd57866d6e17990c25a878e54b6c
luisapcc <00001078481751sp@al.educacao.sp.gov.br>
2024-08-27 14:23:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const avanca = document.querySelectorAll('.bnt-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button =>) {
+avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const 
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
-}
+})
